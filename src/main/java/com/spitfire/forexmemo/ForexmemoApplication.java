4cf905af15852dbd1536f495920b85954ded0b85
JZ_Zhang <zasek415@gmail.com>
2018-05-03 16:35:14 -0400

    Initialization.

---
+++ src/main/java/com/spitfire/forexmemo/ForexmemoApplication.java
@@ -0,0 +1,12 @@
+package com.spitfire.forexmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexmemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForexmemoApplication.class, args);
+    }
+}
