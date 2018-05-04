3f581d540c1e566bf8347839c3668eebf9b04f78
glkwhr <glkwhr@gmail.com>
2018-05-04 18:30:05 -0400

    Added Administrator related code.

---
+++ src/main/java/com/spitfire/forexmemo/domain/Administrator.java
@@ -0,0 +1,89 @@
+package com.spitfire.forexmemo.domain;
+
+import org.springframework.data.annotation.Id;
+
+import java.util.Date;
+import java.util.Objects;
+
+/**
+ * Created by H.W. on 5/3/2018.
+ */
+public class Administrator {
+
+    @Id
+    private String aid;
+
+    private String name;
+    private String password;
+    private String email;
+    private Date createTime;
+    private Date updateTime;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Administrator that = (Administrator) o;
+        return Objects.equals(aid, that.aid) &&
+                Objects.equals(name, that.name) &&
+                Objects.equals(password, that.password) &&
+                Objects.equals(email, that.email) &&
+                Objects.equals(createTime, that.createTime) &&
+                Objects.equals(updateTime, that.updateTime);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(aid, name, password, email, createTime, updateTime);
+    }
+
+    public String getAid() {
+
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+}
